17a931704b08c70021293182a4595b5cb97765a9
Evgeniy Karsiuk <karsiuk0@gmail.com>
2022-04-03 17:19:18 +0300

    site layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
+import React from "react";
 import "./App.css";
-import Header from "./Header";
-import Tehnologies from "./Tehnologies";
-import Footer from "./Footer";
 
 const App = () => {
   return (
-    <div className="App">
-     <Header/>
-     <Tehnologies/>
-     <Footer/>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://ru.seaicons.com/wp-content/uploads/2015/06/Sites-2-icon.png" />
+      </header>
+      <nav className="nav">
+        <div>
+          <a href="#">Profile</a>
+        </div>
+        <div>
+          <a href="#">Messages</a>
+        </div>
+        <div>
+          <a href="#">News</a>
+        </div>
+        <div>
+          <a href="#">Musik</a>
+        </div>
+        <div>
+          <a href="#">Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="" />
+        </div>
+        <div>
+          <img src="" />
+          ava + description
+        </div>
+        <div>
+          my post
+          <div>new post</div>
+        </div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
     </div>
   );
 };
-
-
-
-
 
 export default App;
