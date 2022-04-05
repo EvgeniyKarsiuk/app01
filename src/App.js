eef33d9d0bbcdebcd06a078be60da5d0e89570a2
Evgeniy Karsiuk <karsiuk0@gmail.com>
2022-04-05 22:09:32 +0300

    cssmodule

--- src/App.js
+++ src/App.js
@@ -1,46 +1,16 @@
 import React from "react";
 import "./App.css";
+import Profile from "./components/Profile";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img src="https://ru.seaicons.com/wp-content/uploads/2015/06/Sites-2-icon.png" />
-      </header>
-      <nav className="nav">
-        <div>
-          <a href="#">Profile</a>
-        </div>
-        <div>
-          <a href="#">Messages</a>
-        </div>
-        <div>
-          <a href="#">News</a>
-        </div>
-        <div>
-          <a href="#">Musik</a>
-        </div>
-        <div>
-          <a href="#">Settings</a>
-        </div>
-      </nav>
-      <div className="content">
-        <div>
-          <img src="" />
-        </div>
-        <div>
-          <img src="" />
-          ava + description
-        </div>
-        <div>
-          my post
-          <div>new post</div>
-        </div>
-        <div>
-          <div>post 1</div>
-          <div>post 2</div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from "./Header.module.css";
+
+
+
+const Header = () => {
+  return (
+    <header className={s.header}>
+      <img src="https://ru.seaicons.com/wp-content/uploads/2015/06/Sites-2-icon.png" />
+    </header>
+  );
+};
+
+export default Header;
